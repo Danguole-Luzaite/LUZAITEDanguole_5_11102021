7c57824e0bc9a784010b67518daa899ff25d89b7
Danguole Luzaite <danguole.luzaite@gmail.com>
2021-09-19 23:59:36 +0200

    créé la page de confirmation, supprimer images inutiles, ajouter une boucle for dans index.js, modifications dans le fichier index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,61 +33,56 @@ const listProduct=[
         price: 20,
         description: "My best product 5",
         imgUrl: "/assets/images/oak_5.jpg"  
-      },
-      {
-        id: 6, 
-        name: "Produit 6",
-        price: 20,
-        description: "My best product 6"  
       },  
 ];
 //creation de container de liste des produits
 const containerProduct=document.getElementById("list-product");
 
-/* creation du lien qui englobe un produit */
-const linkProduct=document.createElement("a");
-linkProduct.classList.add('col-12', 'col-sm-6', 'col-md-4', 'mb-3');
-linkProduct.href="/views/pages de produit/page_de_produit-1cross_table.html";
+for (index in listProduct){
+  /* creation du lien qui englobe un produit */
+  const linkProduct=document.createElement("a");
+  linkProduct.classList.add('col-12', 'col-sm-6', 'col-md-4', 'mb-3');
+  linkProduct.href= "/views/pages de produit/page_de_produit-1cross_table.html";
 
-// creation de l'enfants de elements div list product
-containerProduct.appendChild(linkProduct);
+  // creation de l'enfants de elements div list product
+  containerProduct.appendChild(linkProduct);
 
-// creation du block produit
-const divCard=document.createElement("div");
-divCard.classList.add("card");
+  // creation du block produit
+  const divCard=document.createElement("div");
+  divCard.classList.add("card");
 
-// creation de l'enfants de elements a
-linkProduct.appendChild(divCard);
+  // creation de l'enfants de elements a
+  linkProduct.appendChild(divCard);
 
-// creation de l'image
-const imgCardImageTop=document.createElement("img");
-imgCardImageTop.classList.add("card-image-top");
-imgCardImageTop.src=listProduct[0].imgUrl;
-imgCardImageTop.alt="Produit 1";
+  // creation de l'image
+  const imgCardImageTop=document.createElement("img");
+  imgCardImageTop.classList.add("card-image-top");
+  imgCardImageTop.src=listProduct[index].imgUrl;
+  imgCardImageTop.alt="Produit 1";
 
-//creation de l'enfants de element div
-divCard.appendChild(imgCardImageTop);
+  //creation de l'enfants de element div
+  divCard.appendChild(imgCardImageTop);
 
-//creation de element card body
-const divCardBody=document.createElement("div");
-divCardBody.classList.add("card-body");
+  //creation de element card body
+  const divCardBody=document.createElement("div");
+  divCardBody.classList.add("card-body");
 
-// creation de l'enfants de elements div card
-divCard.appendChild(divCardBody);
+  // creation de l'enfants de elements div card
+  divCard.appendChild(divCardBody);
 
-//creation de element h2 card-title
-const h2CardTitle=document.createElement("h2");
-h2CardTitle.classList.add("card-title");
-h2CardTitle.innerText=listProduct[0].description;
+  //creation de element h2 card-title
+  const h2CardTitle=document.createElement("h2");
+  h2CardTitle.classList.add("card-title");
+  h2CardTitle.innerText=listProduct[index].description;
 
-// creation de l'enfants de elements div card body
-divCardBody.appendChild(h2CardTitle);
+  // creation de l'enfants de elements div card body
+  divCardBody.appendChild(h2CardTitle);
 
-//creation de element p card-text
-const pCardText=document.createElement("p");
-pCardText.classList.add("card-text");
-pCardText.innerText=listProduct[0].price +"&euro;";
+  //creation de element p card-text
+  const pCardText=document.createElement("p");
+  pCardText.classList.add("card-text");
+  pCardText.innerText=listProduct[index].price + "  &euro;";
 
-// creation de l'enfants de elements div card body
-divCardBody.appendChild(pCardText);
-
+  // creation de l'enfants de elements div card body
+  divCardBody.appendChild(pCardText);
+}
